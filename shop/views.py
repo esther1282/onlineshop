1774a0f616cc4c030c0e4e5f316b060e3119f77e
estherseo <seojieun1282@gmail.com>
2022-06-30 19:32:33 +0900

    detail

--- shop/views.py
+++ shop/views.py
@@ -6,14 +6,16 @@ def index(request):
     all_products = Product.objects.all()
 
     user = request.user
-
     if request.user.is_authenticated is False:
         user.username = ' '
+
     return render(request, 'shop/index.html', {'all_products': all_products, 'user':user})
 
 def detail(request, product_id):
+    user = request.user
+
     product = get_object_or_404(Product, pk=product_id)
     products_images = ProductImage.objects.filter(product=product)
     #quantity = forms.IntegerField(label=1)
     quantity = 1
-    return render(request, 'shop/detail.html', {'product': product, 'product_images': products_images})
+    return render(request, 'shop/detail.html', {'product': product, 'product_images': products_images, 'user':user})
