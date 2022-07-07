ed8064babe95e766ee0ddefdc7308b167e897040
estherseo <seojieun1282@gmail.com>
2022-07-07 19:10:24 +0900

    FINAL

--- shop/views.py
+++ shop/views.py
@@ -11,7 +11,7 @@ def index(request):
         user.username = ' '
 
     query = request.GET.get("q")
-    if query: #검색할 때 xss 취약하게 설정하기
+    if query:
         search_products = all_products.filter(
             Q(name__icontains=query) | Q(content__icontains=query)
         ).distinct()
